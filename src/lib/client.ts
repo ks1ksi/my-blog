20f9c9a06fefb2ad7fb65acad4e89a1dfd5884aa
ks1ksi <me@ks1ksi.io>
2026-05-01 17:01:06 +0900

    Patch Astro scroll restoration for Samsung Internet

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -23,6 +23,11 @@ const TOC_OPEN_STORAGE_KEY = "article:toc-open";
 const PREVIOUS_PATH_STORAGE_KEY = "navigation:previous-path";
 const RETURN_SCROLL_ROUTES: ReturnScrollRoute[] = [
   {
+    key: "blog",
+    sourcePattern: /^\/blog$/,
+    destinationPattern: /^\/blog\/.+/,
+  },
+  {
     key: "tags",
     sourcePattern: /^\/tags$/,
     destinationPattern: /^\/tags\/[^/]+$/,
